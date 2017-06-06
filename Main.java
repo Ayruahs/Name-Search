f6a1eae606e3e6031a2424c66e10ef2dd0c6fa5f
Shaurya Sinha <shauryasinna@gmail.com>
2017-06-06 23:33:58 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -14,9 +14,6 @@ public class Main{
             int matchRatio;
             final int CONST_MATCH_RATIO = 75;
             final int SOUNDEX_SIM = 4;
-            final int SMALL_DIFF = 4;
-            final int MED_DIFF = 7;
-            final int LARGE_DIFF = 9;
             String exitCharacter = "q";
             String name;
             String[][] secondPassList;
@@ -34,21 +31,13 @@ public class Main{
                 while (sc.hasNext()) {
                     name = sc.nextLine();
 
-                    if (Math.abs(inputName.length() - name.length()) < SMALL_DIFF) {
-                        matchRatio = FuzzySearch.weightedRatio(inputName.toLowerCase(), name.toLowerCase());
-                    }else if (Math.abs(inputName.length() - name.length()) < MED_DIFF){
-                        matchRatio = FuzzySearch.ratio(inputName.toLowerCase(), name.toLowerCase());
-                    }else if(Math.abs(inputName.length() - name.length()) < LARGE_DIFF){
-                        matchRatio = FuzzySearch.partialRatio(inputName.toLowerCase(), name.toLowerCase());
-                    }else{
-                        matchRatio = 0;
-                    }
+                    matchRatio = matchRatio(inputName, name);
 
                     if (matchRatio >= CONST_MATCH_RATIO){
                         i++;
                     }
                 }
-                
+
                 sc.close();
                 secondPassList = new String[i][2];
                 sc = new Scanner(file);
@@ -59,19 +48,13 @@ public class Main{
                 while (sc.hasNext()){
                     name = sc.nextLine();
 
-                    if(Math.abs(inputName.length() - name.length()) < SMALL_DIFF){
-                        matchRatio = FuzzySearch.weightedRatio(inputName.toLowerCase(), name.toLowerCase());
-                    }else if(Math.abs(inputName.length() - name.length()) < MED_DIFF){
-                        matchRatio = FuzzySearch.ratio(inputName.toLowerCase(), name.toLowerCase());
-                    }else if(Math.abs(inputName.length() - name.length()) < LARGE_DIFF){
-                        matchRatio = FuzzySearch.partialRatio(inputName.toLowerCase(), name.toLowerCase());
-                    }else{
-                        matchRatio = 0;
-                    }
+                    matchRatio = matchRatio(inputName, name);
 
                     if (matchRatio >= CONST_MATCH_RATIO){
                         i++;
-                        String output = String.format(i + ") The passenger %s has a %d%% match with %s", inputName, matchRatio, name);
+                        String output = String.format(i + ") The passenger" +
+                                " %s has a %d%% match with %s", inputName,
+                                matchRatio, name);
                         System.out.println(output);
                         secondPassList[i-1][0] = name;
                         secondPassList[i-1][1] = Integer.toString(matchRatio);
@@ -84,7 +67,8 @@ public class Main{
                 //Second pass code from here:
                 int j = 0;
                 for(i=0; i<secondPassList.length; i++){
-                    if (Soundex.compareSoundex(inputName, secondPassList[i][0]) >= SOUNDEX_SIM) {
+                    if (Soundex.compareSoundex(inputName, secondPassList[i][0])
+                            >= SOUNDEX_SIM) {
                         j++;
                     }
                 }
@@ -92,7 +76,8 @@ public class Main{
                 finalList = new String[j];
                 j = 0;
                 for(i=0; i<secondPassList.length; i++){
-                    if (Soundex.compareSoundex(inputName, secondPassList[i][0]) >= SOUNDEX_SIM) {
+                    if (Soundex.compareSoundex(inputName, secondPassList[i][0])
+                            >= SOUNDEX_SIM) {
                         finalList[j] = secondPassList[i][0];
                         j++;
                     }
@@ -100,8 +85,8 @@ public class Main{
 
                 System.out.println("Second Pass results: ");
                 for(i=0; i<finalList.length; i++){
-                    System.out.println(i + 1 + ")" + inputName + " matches with: " + finalList[i]);
-                    System.out.println(Soundex.soundexCode(inputName) + " | " + Soundex.soundexCode(finalList[i]));
+                    System.out.println(i + 1 + ")" + inputName +
+                            " matches with: " + finalList[i]);
                 }
 
                 //Ask for more names or exit
@@ -119,6 +104,24 @@ public class Main{
 
     }
 
+    private static int matchRatio(String string1, String string2){
+        final int SMALL_DIFF = 4;
+        final int MED_DIFF = 7;
+        final int LARGE_DIFF = 9;
+        int matchRatio;
+
+        if(Math.abs(string1.length() - string2.length()) < SMALL_DIFF){
+            matchRatio = FuzzySearch.weightedRatio(string1.toLowerCase(), string2.toLowerCase());
+        }else if(Math.abs(string1.length() - string2.length()) < MED_DIFF){
+            matchRatio = FuzzySearch.ratio(string1.toLowerCase(), string2.toLowerCase());
+        }else if(Math.abs(string1.length() - string2.length()) < LARGE_DIFF){
+            matchRatio = FuzzySearch.partialRatio(string1.toLowerCase(), string2.toLowerCase());
+        }else{
+            matchRatio = 0;
+        }
+
+        return matchRatio;
+    }
 
 
 }
